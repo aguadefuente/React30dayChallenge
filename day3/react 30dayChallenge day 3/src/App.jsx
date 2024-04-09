87e5ecb0f125c5d27d6f3ac89c13bab79106fbc4
lenilaurae <lenilaurae.gmail.com>
2024-04-09 21:30:43 +0200

    images

--- day3/react 30dayChallenge day 3/src/App.jsx
+++ day3/react 30dayChallenge day 3/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import "./App.css";
+import cssImg from "./images/css_logo.png";
+import htmlImg from "./images/html_logo.png";
+import reactImg from "./images/react_logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Hello Vite React App</h1>
+      <h3 style={{ textAlign: "center" }}>Front-End Technologies</h3>
+      <div className="img_container">
+        <img src={htmlImg} alt="htmlimg" />
+        <img src={cssImg} alt="cssimg" />
+        <img src={reactImg} alt="reactimg" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- day3/readme.txt
+++ day3/readme.txt
@@ -2,9 +2,39 @@ APUNTES
 
 - Runtime environment: 
 https://www.codecademy.com/article/introduction-to-javascript-runtime-environments
-
 - Front-end applications: Applications created for and executed in the browser
-- Module: A single or multiple functions, that can be exported and imported when needed
- In React we do not use link to access modules or packages, instead we import the module
+
+
+Exercises: Level 1
+What is a module?
+A single or multiple functions, that can be exported and imported when needed
+In React we do not use link to access modules or packages, instead we import 
+the module
+eg:
+//math.js
+export const addTwo = (a, b) => a + b
+export default function doSomeMath() {...}
+//index.js
+import doSomeMath from './math.js'
+import { addTwo} from './math.js'
+import * as everything from './math.js' // to import everything remaining
+
+What is package?
+is a module or a collection of modules
+
+What is NPM?
+The World's Largest Software Registry (Library)
+https://docs.npmjs.com/about-npm
+
+What is Webpack?
+is a module bundler
+https://webpack.js.org/concepts/
+
+How do you create a new React project?
+I have used Vite https://vitejs.dev/guide/ 
+- npm create vite@latest
+
+What is your favorite code editor (I believe that it is Visual Studio Code)? yes
+Visual Studio Code extensions: eg. prettier, ESLint etc.
 
 
