30209d7654e5136bac57e8f84bbb3bb44ca8ead2
lenilaurae <lenilaurae.gmail.com>
2024-04-16 13:29:28 +0200

    finish day5

--- day5/react-props/src/App.jsx
+++ day5/react-props/src/App.jsx
@@ -1,12 +1,62 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import authorImg from "./images/asabeneh.jpg";
+import "./card.css";
+
+const Card = ({ name, lastname, job, country, skills, image }) => {
+  const style_skills = {
+    backgroundColor: "#ff5722",
+    margin: "5px",
+    width: "50px",
+    height: "20px",
+    borderRadius: "10px",
+    textAlign: "center",
+  };
+
+  const techskills = skills.map((tech) => (
+    <li style={style_skills} key={Math.random()}>
+      {tech}
+    </li>
+  ));
+  console.log(techskills);
+
+  return (
+    <>
+      <div className="card-container">
+        <img src={image} alt="authorImg" />
+        <h2>
+          {name} {lastname}
+        </h2>
+        <p>{country}</p>
+        <p>{job}</p>
+        <h2>Skills</h2>
+        <ul className="skills-container">{techskills}</ul>
+      </div>
+    </>
+  );
+};
 
 function App() {
+  const data = {
+    name: "Asabeneh",
+    lastname: "Yetayeh",
+    job: "Senior Developer",
+    country: "Finland",
+    skills: ["Html", "Css", "Sass", "JScript", "React"],
+    image: authorImg,
+  };
+
   return (
     <>
       <h1>React Props</h1>
+      {/* <Card
+        name={data.name}
+        lastname={data.lastname}
+        county={data.country}
+        job={data.job}
+        image={data.image}
+        skills={data.skills}
+      /> */}
+      <Card {...data} />{" "}
+      {/*para no repetir data. tantas veces, si al final voy a usar todos los datos del objeto */}
     </>
   );
 }
